adcbbedb0c590ea78453c966ecc7c839649f92e5
Srijan Sinha <103751862+Srijandevhub@users.noreply.github.com>
2024-10-14 12:39:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 //complete this code
-class Person {}
-
-class Student extends Person {}
-
-class Teacher extends Person {}
-
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+  get name() {
+    return this._name;
+  }
+  get age() {
+    return this._age;
+  }
+  set age(age) {
+    if (age >= 0) {
+      this._age = age;
+    } else {
+      console.error('Age must be a non-negative number.');
+    }
+  }
+}
+class Student extends Person {
+  study() {
+    console.log(`${this._name} is studying`);
+  }
+}
+class Teacher extends Person {
+  teach() {
+    console.log(`${this._name} is teaching`)
+  }
+}
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
